beeb927291a07a896e187536df9af080c07d1dcc
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-23 15:58:37 +0200

    near

--- jv/script.js
+++ jv/script.js
@@ -1,11 +1,29 @@
-let data;
+// elementi da prendere nel dom
 
+const emailList = document.getElementById("emailList");
+
+// constanti per axios
+let data;
+const randomaizeMail =
+  "https://flynn.boolean.careers/exercises/api/random/mail";
+// ciclo con axios per le 10 email
 for (let i = 0; i < 10; i++) {
-  axios
-    .get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((response) => {
-      data = response;
-      const emailRandom = data.data.response;
-      console.log(emailRandom);
-    });
+  axios.get(randomaizeMail).then((response) => {
+    data = response;
+    const emailRandom = data.data.response;
+    console.log(data.data.response);
+
+    emailList.innerHTML = `<li>${emailRandom}</li>
+    <li>${emailRandom}</li>
+    <li>${emailRandom}</li>
+    <li>${emailRandom}</li>
+    <li>${emailRandom}</li>
+    <li>${emailRandom}</li>
+    <li>${emailRandom}</li>
+    <li>${emailRandom}</li>
+    <li>${emailRandom}</li>
+    <li>${emailRandom}</li>`;
+  });
 }
+
+console.log(emailList);
