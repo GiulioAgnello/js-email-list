7f7806e2ca3a2ee45e77ba46fa4987f2f40a06bb
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-23 16:20:12 +0200

    fix

--- jv/script.js
+++ jv/script.js
@@ -6,24 +6,35 @@ const emailList = document.getElementById("emailList");
 let data;
 const randomaizeMail =
   "https://flynn.boolean.careers/exercises/api/random/mail";
+let emailArray = [];
 // ciclo con axios per le 10 email
 for (let i = 0; i < 10; i++) {
   axios.get(randomaizeMail).then((response) => {
     data = response;
     const emailRandom = data.data.response;
     console.log(data.data.response);
+    emailArray.push(emailRandom);
+    console.log(emailArray);
 
-    emailList.innerHTML = `<li>${emailRandom}</li>
-    <li>${emailRandom}</li>
-    <li>${emailRandom}</li>
-    <li>${emailRandom}</li>
-    <li>${emailRandom}</li>
-    <li>${emailRandom}</li>
-    <li>${emailRandom}</li>
-    <li>${emailRandom}</li>
-    <li>${emailRandom}</li>
-    <li>${emailRandom}</li>`;
+    emailList.innerHTML = `<li>${emailArray[]}</li>
+    `;
   });
 }
 
 console.log(emailList);
+
+// constanti per axios
+// let data;
+// const randomaizeMail =
+//   "https://flynn.boolean.careers/exercises/api/random/mail";
+// ciclo con axios per le 10 email
+
+// axios.get(randomaizeMail).then((response) => {
+//   data = response;
+//   const emailRandom = data.data.response;
+//   console.log(data.data.response);
+
+//   emailList.innerHTML = `<li>${emailRandom}</li>
+    // `;
+// });
+,,.
