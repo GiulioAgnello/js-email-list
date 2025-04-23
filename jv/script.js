ff072d812a554e61ca8886d03d0e0f8090d52344
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-23 16:37:30 +0200

    end-ex

--- jv/script.js
+++ jv/script.js
@@ -1,6 +1,7 @@
 // elementi da prendere nel dom
 
 const emailList = document.getElementById("emailList");
+const myButton = document.getElementById("button");
 
 // constanti per axios
 let data;
@@ -8,21 +9,35 @@ const randomaizeMail =
   "https://flynn.boolean.careers/exercises/api/random/mail";
 let emailArray = [];
 // ciclo con axios per le 10 email
-for (let i = 0; i < 10; i++) {
-  axios.get(randomaizeMail).then((response) => {
-    data = response;
-    const emailRandom = data.data.response;
-    console.log(data.data.response);
-    emailArray.push(emailRandom);
-    console.log(emailArray);
 
-    emailList.innerHTML = `<li>${emailArray[]}</li>
+let genEmail = () => {
+  for (let i = 0; i < 10; i++) {
+    axios.get(randomaizeMail).then((response) => {
+      data = response;
+      const emailRandom = data.data.response;
+
+      emailArray.push(emailRandom);
+      console.log(emailArray);
+
+      emailList.innerHTML = `<li>${emailArray[0]}</li>
+    <li>${emailArray[1]}</li>
+    <li>${emailArray[2]}</li>
+    <li>${emailArray[3]}</li>
+    <li>${emailArray[4]}</li>
+    <li>${emailArray[5]}</li>
+    <li>${emailArray[6]}</li>
+    <li>${emailArray[7]}</li>
+    <li>${emailArray[8]}</li>
+    <li>${emailArray[9]}</li>
     `;
-  });
-}
+    });
+  }
+};
 
-console.log(emailList);
-
+myButton.addEventListener("click", (e) => {
+  e.preventDefault;
+  genEmail();
+});
 // constanti per axios
 // let data;
 // const randomaizeMail =
@@ -35,6 +50,5 @@ console.log(emailList);
 //   console.log(data.data.response);
 
 //   emailList.innerHTML = `<li>${emailRandom}</li>
-    // `;
+// `;
 // });
-,,.
